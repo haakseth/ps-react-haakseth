ff0ad0d8a8bb29527b6c5978ae05cfab4991affa
John Wika Haakseth <johnwika@gmail.com>
2018-05-25 15:47:07 +0200

    Hello world example

--- src/components/HelloWorld/HelloWorld.js
+++ src/components/HelloWorld/HelloWorld.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function HelloWorld({ message }) {
-  return <div>{message}</div>;
+  return <div>Hello {message}</div>;
 }
 
 HelloWorld.propTypes = {
