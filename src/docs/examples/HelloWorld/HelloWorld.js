16eb04f6b8890b5ba776e381b78cc665fbd08e7a
John Wika Haakseth <johnwika@gmail.com>
2018-05-25 21:17:50 +0200

    add index.js for shorter import

--- src/docs/examples/HelloWorld/HelloWorld.js
+++ src/docs/examples/HelloWorld/HelloWorld.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import HelloWorld from 'ps-react/HelloWorld/HelloWorld';
+import HelloWorld from 'ps-react/HelloWorld';
 
 /** Custom message */
 export default function ExampleHelloWorld() {
-  return <HelloWorld message="to you!" />;
+  return <HelloWorld message = "to you!" / > ;
 }
