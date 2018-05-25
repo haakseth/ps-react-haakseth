78968269af371012899c96d0b684b67a4f4da023
John Wika Haakseth <johnwika@gmail.com>
2018-05-25 15:41:35 +0200

    Add docs components

---
+++ src/components/HelloWorld/HelloWorld.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function HelloWorld({ message }) {
+  return <div>{message}</div>;
+}
+
+HelloWorld.propTypes = {
+  /** Message to display */
+  message: PropTypes.string
+};
+
+HelloWorld.defaultProps = {
+  message: 'World'
+};
+
+export default HelloWorld;
