f65a5161ea3253fed0e19bfb40d654d02678b7a8
John Wika Haakseth <johnwika@gmail.com>
2018-06-15 16:25:38 +0200

    add getWidthAsPercentOfTotalWidth pure function

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+// import { getWidthAsPercentOfTotalWidth } from '../../utils/percentUtils';
 
 class ProgressBar extends Component {
   getColor = percent => {
