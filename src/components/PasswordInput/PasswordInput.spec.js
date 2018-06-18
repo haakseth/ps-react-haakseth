464f7edb973ddfd1d197733c9756c55c14b09849
John Wika Haakseth <johnwika@gmail.com>
2018-06-18 17:54:25 +0200

    add interaction test

--- src/components/PasswordInput/PasswordInput.spec.js
+++ src/components/PasswordInput/PasswordInput.spec.js
@@ -1,6 +1,27 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import PasswordInput from './PasswordInput';
+import { shallow } from 'enzyme';
+
+test('toggles input type when show/hide password clicker', () => {
+  const wrapper = shallow(
+    <PasswordInput
+      htmlId="test"
+      name="test"
+      onChange={() => {}}
+      value=""
+      showVisibilityToggle
+    />
+  );
+
+  //password input should be of type password initially
+  expect(wrapper.find({ type: 'password' })).toHaveLength(1);
+  expect(wrapper.find({ type: 'text' })).toHaveLength(0);
+
+  wrapper.find('a').simulate('click');
+  expect(wrapper.find({ type: 'password' })).toHaveLength(0);
+  expect(wrapper.find({ type: 'text' })).toHaveLength(1);
+});
 
 test('hides password quality by default', () => {
   const tree = renderer
